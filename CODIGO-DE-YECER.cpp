9f94a4072c871a2b91413f6be683021f2bf7bccc
gfd <yoscar1995@gmail.com>
2017-04-08 00:30:01 -0430

    ya valide la entrada del menu

--- CODIGO-DE-YECER.cpp
+++ CODIGO-DE-YECER.cpp
@@ -29,31 +29,44 @@ void TextoDecodificar(int n, string & cadena){
 } 
   
 int main() { 
-
-	int  respuesta;
+	int bandera=0,size;
+	string respuesta;
 	do{
-	    cout<<"\t_________________________________________________________\n";
-	    cout<<"\tº CODIFICACION Y DECODIFICACION DE ARCHIVOS!!!!!.        º\n";
-	    cout<<"\tº Seleccione la accion que desee tomar:                  º\n";
-	    cout<<"\tº                                                        º\n";
-	    cout<<"\tº A) Crear un archivo para guardar un mensaje codificado.º\n";
-	    cout<<"\tº B) Decodificar los mensajes ocultos en los archivos.   º\n";
-	    cout<<"\tº C) Salir.                                              º\n";
-	    cout<<"\tº________________________________________________________º\n";
-	    cout<<" Opcion:";
-	    cin>>respuesta;
-		    
+		do{
+		    cout<<"\t_________________________________________________________\n";
+		    cout<<"\tº CODIFICACION Y DECODIFICACION DE ARCHIVOS!!!!!.        º\n";
+		    cout<<"\tº Seleccione la accion que desee tomar:                  º\n";
+		    cout<<"\tº                                                        º\n";
+		    cout<<"\tº A) Crear un archivo para guardar un mensaje codificado.º\n";
+		    cout<<"\tº B) Decodificar los mensajes ocultos en los archivos.   º\n";
+		    cout<<"\tº C) Salir.                                              º\n";
+		    cout<<"\tº________________________________________________________º\n";
+		    cout<<" Opcion:";
+		    cin>>respuesta;
+			size=respuesta.size();
+			if(size>1){
+				cout<<"OPCION INVALIDA"<<endl;
+				bandera=1;						
+			}
 			
-		if(respuesta==1){
-			cout<<"CODIFICAR"<<endl;	
+			else if((respuesta[0]!=97 && respuesta[0]!=98 && respuesta[0]!=99 &&respuesta[0]!=65 && respuesta[0]!=66 &&respuesta[0]!=67)){
+				cout<<"OPCION INVALIDA"<<endl;
+				bandera=1;	
+			}
+		    system("pause");
+		    system("cls");
+		}while(bandera==1);    
+			
+		if(respuesta[0]==97 || respuesta[0]==65){
+			cout<<"CODIFICAR"<<endl;
 		}
 		
-		if(respuesta==2){
+		if(respuesta[0]==98 || respuesta[0]==66){
 			cout<<"DECODIFICAR"<<endl;
 		}
 		
 		
 	
-	}while(respuesta!=3);
+	}while(respuesta[0]!=99 && respuesta[0]!=67);
 	return 0;
 } 
